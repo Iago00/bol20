e053522136edc35f341a2caaf5c7933eeb8f22cd
ipampillonvieitez <ipampillonvieitez@danielcastelao.org>
2020-02-28 09:05:55 +0100

    first commit

---
+++ bol20/src/bol20/Bol20.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol20;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ipampillonvieitez
+ */
+public class Bol20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        ArrayList<Libreria>lista=new ArrayList();
+        ArrayListMetodos obx=new ArrayListMetodos();
+        int opcion;        
+         do{
+            opcion= Integer.parseInt(JOptionPane.showInputDialog("     Menu      "
+                                                             + "\n1.Crear Librp"
+                                                             + "\n2.Amosar"                            
+                                                             + "\n3.Amosar libro Buscado Por  ISBN"
+                                                             + "\n4.Dar de baixa"
+                                                             + "\n5.Sair"));
+            switch(opcion){                
+                case 1:lista.add(obx.crearLibro());break;
+                case 2:obx.amosar(lista);break;
+                case 3:obx.buscarElementoISBN(lista);break;
+                case 4:obx.darBaixa(lista);break;
+                case 5:;break;
+                
+            }
+        }while(opcion!=6);
+    }
+    
+    }
+    
+
